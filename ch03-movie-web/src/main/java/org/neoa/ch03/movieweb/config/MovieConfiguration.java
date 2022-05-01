dbfeca0cdca82655625aa0549e7525b450eb45a2
Cristian Flores <acrixf@gmail.com>
2022-05-01 17:40:19 -0500

    scdf-ch03-ex05 - add MovieEndpoint to handle input channel request

---
+++ ch03-movie-web/src/main/java/org/neoa/ch03/movieweb/config/MovieConfiguration.java
@@ -0,0 +1,10 @@
+package org.neoa.ch03.movieweb.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("META-INF/spring/movie-webapp-integration.xml")
+public class MovieConfiguration {
+
+}
--- ch03-movie-web/src/main/java/org/neoa/ch03/movieweb/model/Movie.java
+++ ch03-movie-web/src/main/java/org/neoa/ch03/movieweb/model/Movie.java
@@ -1,4 +1,32 @@
 package org.neoa.ch03.movieweb.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Movie {
+
+    private String title;
+    private String actor;
+    private int year;
+
+    public String toString(){
+        StringBuilder builder = new StringBuilder();
+        builder.append("Movie(title: ");
+        builder.append(title);
+        builder.append(", actor: ");
+        builder.append(actor);
+        builder.append(", year: ");
+        builder.append(year);
+        builder.append(")");
+        return builder.toString();
+    }
+
 }
