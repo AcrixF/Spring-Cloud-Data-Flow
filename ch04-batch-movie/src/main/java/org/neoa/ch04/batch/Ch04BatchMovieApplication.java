7576da2c7c9a566241ec3dbd04c331358a539cf2
Cristian Flores <acrixf@gmail.com>
2022-05-07 22:01:50 -0500

    scdf-ch04-ex01 - spring batch initial project

---
+++ ch04-batch-movie/src/main/java/org/neoa/ch04/batch/Ch04BatchMovieApplication.java
@@ -0,0 +1,13 @@
+package org.neoa.ch04.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch04BatchMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch04BatchMovieApplication.class, args);
+	}
+
+}
---
+++ ch04-batch-movie/src/test/java/org/neoa/ch04/batch/Ch04BatchMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package org.neoa.ch04.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch04BatchMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
